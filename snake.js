0e68e585362cc2fe3a28a38380c16b2d9bd2a104
Kuuro <noahallart57@gmail.com>
2024-04-10 18:37:10 +0200

    Ajout de la fonction fin_jeu qui arrete le jeu | Modification de la fonction deplacer_perso

--- snake.js
+++ snake.js
@@ -5,6 +5,10 @@
 let canvas_jeu = document.getElementById('jeu_snake');
 let ctx = canvas_jeu.getContext('2d');
 
+let coo_x = 450;
+let coo_y = 450;
+let snake_est_mort = false;
+
 
 
 // ##################################################
@@ -23,6 +27,31 @@ let creer_jeu = () => {
     ctx.fillStyle = 'yellow';
     ctx.arc(450, 450, 15, 0, 2 * Math.PI);
     ctx.fill();
+
+};
+
+
+
+// ##################################################
+//                 FONCTION FIN_JEU
+// ##################################################
+
+let fin_jeu = () => {
+
+    ctx.fillStyle = 'darkblue';
+    ctx.fillRect(0, 0, 1000, 1000);
+
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 10;
+    ctx.strokeRect(2, 2, 896, 896);
+
+    ctx.beginPath();
+    ctx.fillStyle = 'black';
+    ctx.arc(coo_x, coo_y, 15, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+
+    snake_est_mort = true;
 
 };
 
@@ -64,32 +93,31 @@ let ajouter_cercle = (x, y) => {
 //              FONCTION DEPLACER_PERSO
 // ##################################################
 
-let coo_x = 450;
-let coo_y = 450;
-
 let deplacer_perso = (direction) => {
 
     ctx.fillStyle = 'darkblue';
 
-    if (direction === 'haut') {
-        supprimer_cercle(coo_x, coo_y);
-        coo_y -= 30;
-        ajouter_cercle(coo_x, coo_y);
-    }
-    else if (direction === 'bas') {
-        supprimer_cercle(coo_x, coo_y);
-        coo_y += 30;
-        ajouter_cercle(coo_x, coo_y);
-    }
-    else if (direction === 'gauche') {
-        supprimer_cercle(coo_x, coo_y);
-        coo_x -= 30;
-        ajouter_cercle(coo_x, coo_y);
-    }
-    else if (direction === 'droite') {
-        supprimer_cercle(coo_x, coo_y);
-        coo_x += 30;
-        ajouter_cercle(coo_x, coo_y);
+    if (!snake_est_mort) {
+        if (direction === 'haut') {
+            supprimer_cercle(coo_x, coo_y);
+            coo_y -= 30;
+            ajouter_cercle(coo_x, coo_y);
+        }
+        else if (direction === 'bas') {
+            supprimer_cercle(coo_x, coo_y);
+            coo_y += 30;
+            ajouter_cercle(coo_x, coo_y);
+        }
+        else if (direction === 'gauche') {
+            supprimer_cercle(coo_x, coo_y);
+            coo_x -= 30;
+            ajouter_cercle(coo_x, coo_y);
+        }
+        else if (direction === 'droite') {
+            supprimer_cercle(coo_x, coo_y);
+            coo_x += 30;
+            ajouter_cercle(coo_x, coo_y);
+        }
     }
 };
 
@@ -102,20 +130,27 @@ let deplacer_perso = (direction) => {
 creer_jeu();
 
 document.addEventListener("keydown", (event) => {
+
     if (event.key === 'ArrowUp') {
-        console.log('deplacement haut');
         deplacer_perso('haut');
+        console.log(`{x:${coo_x}; y:${coo_y}}`);
     }
     else if (event.key === 'ArrowDown') {
-        console.log('deplacement bas');
         deplacer_perso('bas');
+        console.log(`{x:${coo_x}; y:${coo_y}}`);
     }
     else if (event.key === 'ArrowLeft') {
-        console.log('deplacement gauche');
         deplacer_perso('gauche');
+        console.log(`{x:${coo_x}; y:${coo_y}}`);
     }
     else if (event.key === 'ArrowRight') {
-        console.log('deplacement droite');
         deplacer_perso('droite');
+        console.log(`{x:${coo_x}; y:${coo_y}}`);
     }
+
+
+    if (coo_x < 30 || coo_x > 870 || coo_y < 30 || coo_y > 870) {
+        fin_jeu();
+    }
+
 });
